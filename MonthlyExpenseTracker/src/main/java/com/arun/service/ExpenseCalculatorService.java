1c31695663942fb8fc43c5cf35acc7999b06248d
aruntanakam <aruntanakam2000@gmail.com>
2024-02-29 17:02:38 +0530

    added try and catch

--- MonthlyExpenseTracker/src/main/java/com/arun/service/ExpenseCalculatorService.java
+++ MonthlyExpenseTracker/src/main/java/com/arun/service/ExpenseCalculatorService.java
@@ -1,5 +1,10 @@
 package com.arun.service;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 import org.springframework.stereotype.Service;
 
 @Service
@@ -7,7 +12,12 @@ public class ExpenseCalculatorService {
 	
 	public static void calculateExpense()
 	{
-		
+		try(BufferedReader br=new BufferedReader(new FileReader(""))) {
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
